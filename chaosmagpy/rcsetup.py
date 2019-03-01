deabef73516016f35bc7a8d19d506033743d5c1a
ancklo <ancklo@space.dtu.dk>
2019-03-01 23:26:02 +0100

    Added new module that will contain settings of ChaosMagPy.

---
+++ chaosmagpy/rcsetup.py
@@ -0,0 +1,11 @@
+import os
+
+
+defaultParams = {
+    'reference_radius': 6371.2,  # placeholder
+
+    'RC_index_file': os.path.join('lib', 'RC_builtin.h5'),  # placeholder
+    'RC_index_fallback': os.path.join('lib', 'RC_builtin.h5'),  # placeholder
+
+    'CHAOS_version': '6.x7'  # placeholder
+}
