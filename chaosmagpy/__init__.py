e96adf3ee790ad620019a15055d3865dbd7caa92
ancklo <ancklo@space.dtu.dk>
2019-03-15 16:54:26 +0100

    Added configCHAOS.

--- chaosmagpy/__init__.py
+++ chaosmagpy/__init__.py
@@ -1,11 +1,11 @@
 from chaosmagpy.chaos import CHAOS, load_CHAOS_matfile, load_CHAOS_shcfile
+from chaosmagpy.config_utils import configCHAOS
 import chaosmagpy.model_utils
 import chaosmagpy.plot_utils
 import chaosmagpy.coordinate_utils
 import chaosmagpy.data_utils
-import chaosmagpy.config_utils
 
 
-__all__ = ["CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile"]
+__all__ = ["CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile", "configCHAOS"]
 
 __version__ = "0.1a4"
