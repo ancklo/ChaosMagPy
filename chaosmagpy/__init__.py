f107787b2269c6f59aa8a84625001d95f3ccf6a3
ancklo <ancklo@space.dtu.dk>
2021-07-02 11:41:56 +0200

    Changed init statements to use relative path.

--- chaosmagpy/__init__.py
+++ chaosmagpy/__init__.py
@@ -1,11 +1,13 @@
-from chaosmagpy.chaos import (CHAOS, load_CHAOS_matfile, load_CHAOS_shcfile,
-                              load_CovObs_txtfile, load_gufm1_txtfile)
-from chaosmagpy.config_utils import basicConfig
-from chaosmagpy.model_utils import synth_values
+from .chaos import (CHAOS, load_CHAOS_matfile, load_CHAOS_shcfile,
+                    load_CovObs_txtfile, load_gufm1_txtfile)
+from .config_utils import basicConfig
+from .model_utils import synth_values
 
 
-__all__ = ["CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile",
-           "load_CovObs_txtfile", "load_gufm1_txtfile",
-           "basicConfig", "synth_values"]
+__all__ = [
+    "CHAOS", "load_CHAOS_matfile", "load_CHAOS_shcfile",
+    "load_CovObs_txtfile", "load_gufm1_txtfile",
+    "basicConfig", "synth_values"
+]
 
 __version__ = "0.7-dev"
